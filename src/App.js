499d653c48dc5473bba3c2351b3e8e21040ae336
yaspyrih <yaspyrih@gmail.com>
2023-10-10 22:50:26 -0300

    some JS functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ['/', '*', '+', '-', '.'];
+
+  const updateCalc = value => {
+    setCalc(calc + value);
+  }
   const createDigits = () =>{
     const digits = [];
 
     for(let i = 1; i<10; i++) {
       digits.push(
-        <button key={i}>{i}</button>
+        <button 
+            onClick={() => updateCalc(i.toString())} 
+            key={i}>
+            {i}
+        </button>
       )
     }
     return digits;
@@ -13,13 +27,14 @@ function App() {
     <div className="App">
       <div className="calculator">
         <div className="display">
-            <span>(0)</span>0
+            {result ? <span>(0)</span> : '' }
+            { calc || "0" }
           </div>
           <div className="operators">
-            <button>/</button>
-            <button>*</button>
-            <button>+</button>
-            <button>-</button>
+            <button onClick={() => updateCalc('/')}>/</button>
+            <button onClick={() => updateCalc('*')}>*</button>
+            <button onClick={() => updateCalc('+')}>+</button>
+            <button onClick={() => updateCalc('-')}>-</button>
             
             <button>DEL</button>
           
@@ -27,8 +42,9 @@ function App() {
         
         <div className="digits">
           { createDigits() }
-          <button>0</button>
-          <button>.</button>
+          <button onClick={() => updateCalc('0')}>0</button>
+          <button onClick={() => updateCalc('.')}>.</button>
+
           <button>=</button>
         </div>
       </div>
