c19160be7d0ff6c466fea95333cd37775ec00cbd
yaspyrih <yaspyrih@gmail.com>
2023-10-12 16:30:01 -0300

    build

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
 
   const updateCalc = value => {
     if (
-       ops.includes(value) && calc === '' ||
-       ops.includes(value) && ops.includes(calc.slice(-1)
+       (ops.includes(value) && calc === '' )||
+       (ops.includes(value) && ops.includes(calc.slice(-1))
        )
     ) {
       return;
