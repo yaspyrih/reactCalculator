3fa34ca086def7b82b8aed6b79218d0a5700f926
yaspyrih <yaspyrih@gmail.com>
2023-10-12 19:25:54 -0300

    eslint-disable-next-line

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
     setCalc(calc + value);
 
     if(!ops.includes(value)) {
+      //eslint-disable-next-line
       setResult(eval(calc + value).toString());
     }
   }
@@ -37,6 +38,7 @@ function App() {
   }
 
   const calculate = () => {
+    //eslint-disable-next-line
     setCalc(eval(calc).toString());
   }
 
